851f377c2ee87bf0e4c1009df9f89e994825fd1f
Oliver Castillo <ocastilt@nttdata.com>
2024-11-26 20:39:48 -0500

    chore: initial commit

---
+++ src/main/java/com/bank/creditservice/BankCreditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.creditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCreditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCreditServiceApplication.class, args);
+	}
+
+}
